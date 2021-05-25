37b9333b541e78b114373279c282be990d6ce93e
maxtenix <56773733+maxtenix@users.noreply.github.com>
2021-05-25 21:50:15 +0200

    Add files via upload

---
+++ HTML/CWWS-Prototyp/script.js
@@ -0,0 +1,11 @@
+//active navigation bar
+const currentLocation = location.href;
+const menuItem= document.querySelectorAll('a');
+const menuLength = menuItem.length
+for(let i=0;i<menuLength;i++){
+    if(menuItem[i].href ===currentLocation){
+        menuItem[i].parentNode.className ="nav-link-wrapper active-nav-link"
+    }
+}
+
+//
