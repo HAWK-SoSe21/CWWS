0f6ed49f49ce02417c95de9f77b8d48c5b8544ca
maxtenix <56773733+maxtenix@users.noreply.github.com>
2021-06-10 12:24:03 +0200

    Add files via upload

---
+++ HTML/CWWS/HTML/CWWS/footer.php
@@ -0,0 +1,12 @@
+        <div class="footer">
+            <div class="navbarfooter">
+                <p class="copyright">©HAWK</p>
+                <li><a href="contact.php">Kontakt</a></li>
+                <li><a href="impressum.php">Impressum</a></li>
+                <li><a href="datalaw.php">Datenschutz</a></li>
+            </div>
+        </div>
+        
+        <script src="script.js"></script>
+    </body>
+</html>
