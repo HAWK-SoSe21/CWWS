173e381b1e8a17dc6762d14896cce37d6c4b18df
maxtenix <56773733+maxtenix@users.noreply.github.com>
2021-06-10 12:15:47 +0200

    alte version gelöscht

--- HTML/CWWS/preferences.php
+++
@@ -1,9 +0,0 @@
-<?php include_once 'header.php';?>
-
-            <div class="main">
-                <div class="mainbox">
-                    <li><a href="phpmyadmin/">PHPMyAdmin</a></li>
-                </div>
-            </div>
-
-<?php include_once 'footer.php';?>
